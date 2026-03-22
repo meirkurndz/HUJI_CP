d1fbfef8f30c91eac9d1339bd32fa397c3febf7c
Meir K <meir.kurndz@gmail.com>
2026-03-22 15:45:03 +0200

    Q_A solution

--- lesson_1/Q_A.cpp
+++ lesson_1/Q_A.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+typedef long long ll;
+
+using namespace std;
+
+
 int main() {
-    std::cout << "Hello Lesson 1!" << std::endl;
-    return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll W = 0;
+
+    while(cin >> W){
+        ll N = 0;
+        cin >> N;
+        ll area =0;
+        for(int i = 0; i < N; i++){
+            ll w,l;
+            cin >> w >>l;
+            area += w*l;
+        }
+        ll L = (area/W);
+        cout << L << "\n";
+    }
 }
