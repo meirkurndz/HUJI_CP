4e81682da1b3a7a17c2a0f99a5c8fda2ef11c66a
Meir K <meir.kurndz@gmail.com>
2026-04-28 13:38:12 +0300

    lesson 1 + 3

---
+++ lesson_3/P_A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll; 
+
+int largest_rect(vll hist){
+    hist.push_back(0);
+
+    stack<ll> S;
+    ll max_area = 0;
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    cin >> T;
+
+    while(--T){
+
+
+
+    }
+
+}
